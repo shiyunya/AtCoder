e43e2ed8ce0b6e63b0b0a41b0a4bd7e89971a88f
Syunya Suzuki <syunya_0617@outlook.jp>
2021-04-01 15:47:53 +0900

    change default files

--- atcoder3.py
+++ atcoder3.py
@@ -9,39 +9,18 @@ mod = pow(10,9)+7
 import sys
 sys.setrecursionlimit(2147483647)
 import math
-from itertools import accumulate
-from itertools import permutations
-from collections import Counter
-from collections import deque
-from collections import defaultdict
 import bisect
 import heapq
 import re
-inf = float('inf')
-dic = defaultdict(list)#lambda:0)
-#from decimal import Decimal
+from itertools import accumulate
+from itertools import permutations
 from itertools import combinations
+from collections import Counter
+from collections import deque
+from collections import defaultdict
+from decimal import Decimal
+inf = float('inf')
+dic = defaultdict(lambda:0)
 
 n = N()
-
-mm = [[inf,-inf] for i in range(n+1)]
-cs = set()
-for i in range(n):
-    x,c = L()
-    mm[c][0] = min(mm[c][0],x)
-    mm[c][1] = max(mm[c][1],x)
-    cs.add(c)
-dp = [[0,0]]
-cs = sorted(list(cs))
-p = 0
-mm[0][0] = mm[0][1] = 0
-#print(mm[:4])
-for i in range(len(cs)):
-    d = min(dp[i][1] + abs(mm[p][1] - mm[cs[i]][1]) ,dp[i][0] + abs(mm[p][0] - mm[cs[i]][1])) + abs(mm[cs[i]][1] - mm[cs[i]][0])
-    u = min(dp[i][1] + abs(mm[p][1] - mm[cs[i]][0]) ,dp[i][0] + abs(mm[p][0] - mm[cs[i]][0])) + abs(mm[cs[i]][1] - mm[cs[i]][0])
-    p = cs[i]
-    #print(p)
-    dp.append([d,u])
-#print(dp)
-#print(dp[i],mm[p])
-print(min(dp[-1][1] + abs(mm[p][1]) , dp[-1][0] + abs(mm[p][0])))
+print(n)
